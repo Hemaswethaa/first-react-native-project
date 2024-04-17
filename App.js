a4b0ab71e7885bb20df1ea81c064a2fb2db4c031
Hemaswethaa <166999348+Hemaswethaa@users.noreply.github.com>
2024-04-17 22:47:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<h1> Hello World!
+      <br/>
+      I am hema swethaa, this is my first project on react. </h1>
+    
+	);
+}
+
+export default App;
